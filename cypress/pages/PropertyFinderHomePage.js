afa9707d417c2e49b8da988339fb691c5026f7dd
sddkgkts <goktas.sddk@gmail.com>
2023-10-31 09:24:34 +0300

    second Test scenario added

--- cypress/pages/PropertyFinderHomePage.js
+++ cypress/pages/PropertyFinderHomePage.js
@@ -5,21 +5,14 @@ class PropertyFinderHomePage {
     dropDownMenuForProperty: () => cy.get('div.dd__head.dd--normal.dd--secondary.dd--filled').eq(1),
     selectItemFromDropdownMenuForPropertyType: (itemType) => cy.get('button.dropdown-list__item-content.dropdown-list__item-button').contains(itemType),
     dropDownMenuForPrice: () => cy.get('div.price-selector').eq(1),
-
-
     findPriceBox: (priceType) => cy.get('span.text-field__placeholder-content')
-    .contains(priceType)
-    .parent('.text-field__placeholder')
-    .next('.input2')
-    .find('input.text-field__input.range-selector__input'),
-
-
-    findSearchButton: () => cy.get('button.button-2.button-primary.filter-form-search-button.filter-form-component-variant__m-hide',{ timeout: 10000 }),
-
+      .contains(priceType)
+      .parent('.text-field__placeholder')
+      .next('.input2')
+      .find('input.text-field__input.range-selector__input'),
+    findSearchButton: () => cy.get('button.button-2.button-primary.filter-form-search-button.filter-form-component-variant__m-hide', { timeout: 10000 }),
+    findCommercialCheckBox: () => cy.get('span.checkbox-component__box'),
   };
-
- 
-
 
   clickButtonAndCheckIsSelected(options) {
     this.elements.findButton(options).click();
@@ -31,28 +24,25 @@ class PropertyFinderHomePage {
     this.elements.selectItemFromDropdownMenuForPropertyType(itemType).click()
   }
 
-
-  typeMaxAndMinPrice(minPrice, maxPrice ) {
+  typeMaxAndMinPrice(minPrice, maxPrice) {
     const priceTypes = [{ 'Min. Price (BHD)': minPrice }, { 'Max. Price (BHD)': maxPrice }];
     this.elements.dropDownMenuForPrice().click();
     for (const priceType of priceTypes) {
       const key = Object.keys(priceType)[0];
-      if ( priceType[key] !== "") {
+      if (priceType[key] !== "") {
         this.elements.findPriceBox(key).click().type(priceType[key])
       }
     }
-    this.elements.findSearchButton().click()
-
+    this.clickSearchButton();
   }
 
+  clickCommercialCheckBox() {
+    this.elements.findCommercialCheckBox().click();
+  }
 
-
-
-
-
-
-
-
+  clickSearchButton() {
+    this.elements.findSearchButton().click()
+  }
 
 
 }
